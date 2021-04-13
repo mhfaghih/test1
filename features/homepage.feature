e2c3bef2f9371f1ee59f8071fe8dffed8547ac21
Hoss Faghih <mhfaghih@outlook.com>
2021-04-13 19:43:52 -0400

    added basic testing functionality with behave

---
+++ features/homepage.feature
@@ -0,0 +1,8 @@
+Feature: homepage running
+    Scenario: Homepage loads and returns 200
+        Given a server
+        When the website is called
+        Then the homepage loads and returns 200
+
+
+
---
+++ features/steps/steps.py
@@ -0,0 +1,13 @@
+from behave import *
+
+@given("a server")
+def step_impl(context):
+    pass
+
+@when("the website is called")
+def step_impl(context):
+    pass
+
+@then("the homepage loads and returns 200")
+def step_impl(context):
+    pass
